1b38b8e2b38a617a1c2a632e1fdf146c667654c4
Erin Johnson <erinjohnson47@gmail.com>
2019-09-03 13:42:40 -0700

    fix typo in db file

--- db/db.js
+++ db/db.js
@@ -1,19 +1,21 @@
 const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost/pantry';
 
-mongoose.connect('mongodb://localhost/pantry', {
+
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
 
-mongoose.connection.on('connect', () => {
-    console.log('Hello, Mongoose is connected')
+mongoose.connection.on('connected', () => {
+    console.log(`Hello, Mongoose is connected to ${connectionString}`)
 })
 
 mongoose.connection.on('error', (err) => {
-    console.log(err, ' mongoose failed to connect')
+    console.log(err, `mongoose error: ${err}`)
 });
 
-mongoose.connection.on('disconncted', () => {
-    console.log('Mongoose is disconnected')
+mongoose.connection.on('disconnected', () => {
+    console.log(`Mongoose is disconnected from ${connectionString}`)
 });  
